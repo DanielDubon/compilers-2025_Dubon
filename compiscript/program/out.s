e1e151a0903292ebf49e6c7f9c0b569cfdb25b16
guzmanalejandra <guz20262@uvg.edu.gt>
2025-11-14 17:16:42 -0600

    feat: tried running on mips

--- compiscript/program/out.s
+++ compiscript/program/out.s
@@ -2,43 +2,33 @@
 .text
 .globl main
 
-# --- function foo ---
-foo:
+# --- function sum ---
+sum:
+  addi $sp, $sp, -32
+  sw   $ra, 28($sp)
+  sw   $fp, 24($sp)
+  move $fp, $sp
+  add  $t0, $a0, $a1
+  move $v0, $t0
+  lw   $fp, 24($sp)
+  lw   $ra, 28($sp)
+  addi $sp, $sp, 32
+  jr   $ra
+
   addi $sp, $sp, -36
   sw   $ra, 32($sp)
   sw   $fp, 28($sp)
   move $fp, $sp
   sw   $s0, 24($sp)
-  lw   $s0, 36($fp)
-  add  $t0, $a0, $a1
-  add  $t1, $t0, $a2
-  add  $t2, $t1, $a3
-  add  $t3, $t2, $s0
-  move $v0, $t3
+  li   $s0, 0
+  li   $a0, 2
+  li   $a1, 3
+  jal  sum
+  move $t1, $v0
+  move $s0, $t1
+  move $v0, $s0
   lw   $s0, 24($sp)
   lw   $fp, 28($sp)
   lw   $ra, 32($sp)
   addi $sp, $sp, 36
-  jr   $ra
-
-# --- function main ---
-main:
-  addi $sp, $sp, -32
-  sw   $ra, 28($sp)
-  sw   $fp, 24($sp)
-  move $fp, $sp
-  li   $a0, 1
-  li   $a1, 2
-  li   $a2, 3
-  li   $a3, 4
-  li $t9, 5
-  addi $sp, $sp, -4
-  sw $t9, 0($sp)
-  jal  foo
-  addi $sp, $sp, 4
-  move $t4, $v0
-  move $v0, $t4
-  lw   $fp, 24($sp)
-  lw   $ra, 28($sp)
-  addi $sp, $sp, 32
   jr   $ra
